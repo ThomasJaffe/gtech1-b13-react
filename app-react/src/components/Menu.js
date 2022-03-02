063c3006513c93bddfc3ca9b97932c5f2dce63d8
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-02 12:47:43 +0100

    Update Menu.js

--- app-react/src/components/Menu.js
+++ app-react/src/components/Menu.js
@@ -1,8 +1,9 @@
 import {Navbar, Nav, NavDropdown} from 'react-bootstrap';
+import {Link} from 'react-router-dom'
 
 function Menu(){
     return <Navbar expand="lg">
-      <Navbar.Brand href="#home">Les Boiboites</Navbar.Brand>
+      <Navbar.Brand href="/">Les Boiboites</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="">

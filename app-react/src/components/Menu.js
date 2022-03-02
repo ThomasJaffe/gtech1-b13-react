1d6635fd22c1cc634b15d7e8a422396c765d32b3
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-02 10:20:54 +0100

    Update Menu.js

--- app-react/src/components/Menu.js
+++ app-react/src/components/Menu.js
@@ -3,19 +3,19 @@ import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap';
 function Menu(){
     return <Navbar expand="lg">
     <Container>
-      <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+      <Navbar.Brand href="#home">Les Boiboites</Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="me-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#link">Link</Nav.Link>
-          <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+          <NavDropdown title="Produits" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Produit 1</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.2">Produit 2</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">Produit 3</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.4">Produit 4</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.5">Produit 5</NavDropdown.Item>
           </NavDropdown>
+          <Nav.Link href="#home">À propos</Nav.Link>
+        <Nav.Link href="#link">Panier</Nav.Link>
         </Nav>
       </Navbar.Collapse>
     </Container>

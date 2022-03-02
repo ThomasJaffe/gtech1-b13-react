567c2ecf11f4f97849fae4d295cff3d7eb6cfa3b
Darkarva <achabre@gaming.tech>
2022-03-02 12:29:57 +0100

    Update Accueil.js

--- app-react/src/Accueil.js
+++ app-react/src/Accueil.js
@@ -12,19 +12,19 @@ class Accueil extends Component {
     <div class="types">
         <h1>Différents types de boites disponibles :</h1>
         <h2>- Boites mystères Classique :</h2>
-        <p>Contenu des boites Classique</p>
-        <h2>- Boites mystères Gamer :</h2>
-        <p>Contenu des boites Gamer</p>
+        <p>Contenu : Tout et n'importe quoi! La surprise à l'état pur!</p>
+        <h2>- Boites mystères Gamer : </h2>
+        <p>Contenu : Pour vous les gamers, matériel informatique, composants électroniques, et jeux vidéos pour de longues heures de fun!</p>
         <h2>- Boites mystères Repas :</h2>
-        <p>Contenu des boites Repas</p>
+        <p>Contenu : Pour les gourmets ou simple amateurs culinaire, des denrées diverse et variées (et bien conservé) de quoi régaler les papilles!</p>
         <h2>- Boites mystères Bricolage :</h2>
-        <p>Contenu des boites Bricolage</p>
+        <p>Contenu : </p>
         <h2>- Boites mystères Artiste :</h2>
-        <p>Contenu des boites Artiste</p>
+        <p>Contenu : </p>
         <h2>- Boites mystères Meuble :</h2>
-        <p>Contenu des boites Meuble</p>
+        <p>Contenu : </p>
         <h2>- Boites mystères Cocktail :</h2>
-        <p>Contenu des boites Cocktail</p>
+        <p>Contenu : </p>
     </div>
     </>
     );

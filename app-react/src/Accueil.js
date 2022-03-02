d3c4032a206036e2a05caf470f9b1b42955d5a41
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-02 10:12:04 +0100

    added navbar

---
+++ app-react/src/Accueil.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Menu from './components/Menu';
+
+class Accueil extends Component {
+  render() {
+    return (
+      <div>
+       <Menu />
+      </div>
+    );
+  }
+}
+
+export default Accueil;

287396397e29375c965fdc92c325eec0000d3263
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-02 12:11:56 +0100

    added main menu

--- app-react/src/Accueil.js
+++ app-react/src/Accueil.js
@@ -9,8 +9,22 @@ class Accueil extends Component {
     <div>
     <Menu />
     </div>
-    <div>
-        
+    <div class="types">
+        <h1>Différents types de boites disponibles :</h1>
+        <h2>- Boites mystères Classique :</h2>
+        <p>Contenu des boites Classique</p>
+        <h2>- Boites mystères Gamer :</h2>
+        <p>Contenu des boites Gamer</p>
+        <h2>- Boites mystères Repas :</h2>
+        <p>Contenu des boites Repas</p>
+        <h2>- Boites mystères Bricolage :</h2>
+        <p>Contenu des boites Bricolage</p>
+        <h2>- Boites mystères Artiste :</h2>
+        <p>Contenu des boites Artiste</p>
+        <h2>- Boites mystères Meuble :</h2>
+        <p>Contenu des boites Meuble</p>
+        <h2>- Boites mystères Cocktail :</h2>
+        <p>Contenu des boites Cocktail</p>
     </div>
     </>
     );

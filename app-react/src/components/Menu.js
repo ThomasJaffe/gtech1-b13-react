fd78797b0dc4ceaa321cae36ebefab416732f634
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-09 15:39:52 +0100

    menu fix

--- app-react/src/components/Menu.js
+++ app-react/src/components/Menu.js
@@ -19,7 +19,7 @@ function Menu(){
             <Link className="dropdown-item" to="/boitecocktail">Boites mystères Cocktail</Link>
           </NavDropdown>
         <Link className="nav-link" to="/apropos">À propos</Link>
-        <Link className="nav-link" to="/panier" float="right"> <img src="http://localhost:1337/uploads/panier_eda1084066.png?updated_at=2022-03-04T11:28:05.517Z"></img></Link>
+        <Link className="nav-link panier" to="/panier" float="right"> <img src="http://localhost:1337/uploads/panier_eda1084066.png?updated_at=2022-03-04T11:28:05.517Z"></img></Link>
         </Nav>
       </Navbar.Collapse>
     </Navbar>

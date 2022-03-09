b0d80bf92794056255221d3776907767cd2d3cb3
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-09 15:35:32 +0100

    menu fix

--- app-react/src/components/Menu.js
+++ app-react/src/components/Menu.js
@@ -7,9 +7,9 @@ function Menu(){
     <Navbar expand="lg" sticky="top">
       <Link className='home' to="/" >Les Boiboites</Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <Navbar.Collapse id="basic-navbar-nav ">
         <Nav className="">
-          <NavDropdown title="Produits" id="basic-nav-dropdown">
+          <NavDropdown title="Produits" id="basic-nav-dropdown" className="droplink">
             <Link className="dropdown-item" to="/boiteclassique">Boites mystères Classique</Link>
             <Link className="dropdown-item" to="/boitegamer">Boites mystères Gamer</Link>
             <Link className="dropdown-item" to="/boiterepas">Boites mystères Repas</Link>
@@ -18,7 +18,7 @@ function Menu(){
             <Link className="dropdown-item" to="/boitemeuble">Boites mystères Meuble</Link>
             <Link className="dropdown-item" to="/boitecocktail">Boites mystères Cocktail</Link>
           </NavDropdown>
-        <Link className="nav-link" to="/apropos">À propos</Link>
+        <Link className="nav-link links" to="/apropos">À propos</Link>
         <Link className="nav-link" to="/panier" float="right"> <img src="http://localhost:1337/uploads/panier_eda1084066.png?updated_at=2022-03-04T11:28:05.517Z"></img></Link>
         </Nav>
       </Navbar.Collapse>

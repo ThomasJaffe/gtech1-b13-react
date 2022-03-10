fd5f5f821aa291b25dc029b51f726315d15ac2dc
Thomas Jaffe <tjaffe@gaming.tech>
2022-03-10 11:44:09 +0100

    added picture

--- app-react/src/components/MyCard.js
+++ app-react/src/components/MyCard.js
@@ -1,9 +1,10 @@
 import {Card,Button} from 'react-bootstrap';
+import Boite from './Boite mystere.png';
 
 function MyCard(props){
     return <div className="d-flex justify-content-around">
     {props.articles.map((u,i) =><Card key={i} style={{ width: '25rem', marginRight:"2rem", marginLeft:"2rem" }}>
-      <Card.Img variant="top"/>
+      <Card.Img variant="top" src={Boite}/>
       <Card.Body>
         <Card.Title>{u.attributes.name}</Card.Title>
         <Card.Text>
